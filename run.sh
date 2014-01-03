0d950344b4d1d3303f0efd9d40ee533f095e7f83
Anders Norman <anders.norman@barco.com>
2014-01-03 10:54:18 +0100

    Updated run.sh for Fredrikstad/Norway

--- run.sh
+++ run.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # Firefox settings
-FF_PROFILE="example"
-FF_PROFILE_DIR="et9az2vp.example"
-FF_WAIT=42
-FF_URL="http://www.ingress.com/intel?ll=49.884386,10.89515&z=14"
+FF_PROFILE="ingress_fredrikstad"
+FF_PROFILE_DIR="1i9rkt4s.ingress_fredrikstad"
+FF_WAIT=200
+FF_URL="http://www.ingress.com/intel?ll=59.209271,10.934658&z=15"
 
 # Virtual X Server Settings
-XVFB_RES_WIDTH=1920
-XVFB_RES_HEIGHT=1080
+XVFB_RES_WIDTH=2560
+XVFB_RES_HEIGHT=1600
 XVFB_DISPLAY=23
 
 # Interval between screenshots
-SCREENSHOT_INTERVAL=300
+SCREENSHOT_INTERVAL=5
 
 # Dimensions of screenshot (cropped)
-SCREENSHOT_WIDTH=1280
-SCREENSHOT_HEIGHT=720
+SCREENSHOT_WIDTH=1920
+SCREENSHOT_HEIGHT=1080
 
 # Crop offset for screenshot
-SCREENSHOT_OFFSET_LEFT=220
-SCREENSHOT_OFFSET_TOP=220
+SCREENSHOT_OFFSET_LEFT=335
+SCREENSHOT_OFFSET_TOP=245
 
 echo "Staring XVFB on $XVFB_DISPLAY"
 Xvfb :${XVFB_DISPLAY} -screen 0 ${XVFB_RES_WIDTH}x${XVFB_RES_HEIGHT}x24 -noreset -nolisten tcp 2> /dev/null &
